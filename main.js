c5239e25c887c68379169f4baa843ad67e9cbef3
Hero <caleb@calebgj.io>
2019-05-03 12:22:11 -0500

    add code

---
+++ main.js
@@ -0,0 +1,28 @@
+if (window.location.href != "https://hac40.esp.k12.ar.us/HomeAccess40/Content/Student/Assignments.aspx") {
+  alert("You need to be in the direct page, not the class page or any other page. Redirecting to the direct page. You will need to rerun the code.")
+  window.location.href = "https://hac40.esp.k12.ar.us/HomeAccess40/Content/Student/Assignments.aspx"
+} else {
+  setTimeout(function(){document.getElementsByClassName("sg-header")[0].innerHTML = document.getElementsByClassName("sg-header")[0].innerHTML + '<button id="plnMain_btnCollapseExpand" type="button" class="sg-button sg-right sg-5px-margin btnCollapseExpand ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only" title="Show assignments that you have Ns or Zs in." onclick="javascript:main();" role="button"><span class="ui-button-text">NZ Grade Check</span></button>'},100)
+}
+var notTurnedIn = []
+function main() {
+  for (var i = 0; i < document.getElementsByClassName("AssignmentClass").length; i++) {
+    for (var j = 0; j < document.getElementsByClassName("AssignmentClass")[i].getElementsByTagName("tr").length-6; j++) {
+      if (document.getElementsByClassName("AssignmentClass")[i].getElementsByTagName("tr")[j].getElementsByTagName("td")[4].innerText == "N") {
+        notTurnedIn.push(document.getElementsByClassName("AssignmentClass")[i].getElementsByTagName("tr")[j].getElementsByTagName("td")[2].innerText+": "+document.getElementsByClassName("AssignmentClass")[i].getElementsByTagName("tr")[j].getElementsByTagName("td")[4].innerText)
+      }
+      if (document.getElementsByClassName("AssignmentClass")[i].getElementsByTagName("tr")[j].getElementsByTagName("td")[4].innerText == "Z") {
+        notTurnedIn.push(document.getElementsByClassName("AssignmentClass")[i].getElementsByTagName("tr")[j].getElementsByTagName("td")[2].innerText+": "+document.getElementsByClassName("AssignmentClass")[i].getElementsByTagName("tr")[j].getElementsByTagName("td")[4].innerText)
+      }
+    }
+  }
+  setTimeout(function(){formatAlert()},100)
+}
+function formatAlert() {
+  var output = "<div>Assignments that you have N/Zs in:</div>"
+  for (var i = 0; i < notTurnedIn.length-1; i++) {
+    output = output + "<div>" + notTurnedIn[i] + "</div>"
+  }
+  output = output + notTurnedIn[notTurnedIn.length-1]
+  SunGard.Common.ShowDialog(output, "NZ Grade Check", undefined, 750, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, true, 500);
+}
